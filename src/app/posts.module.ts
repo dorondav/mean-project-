327d114abe1a412c291b023d639cf572548a481a
doron davidowitz <doron@evisame.com>
2020-01-29 13:07:02 +0200

    first commit

---
+++ src/app/posts.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from './angular-material.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    declarations: [
+        PostCreateComponent,
+        PostListComponent,
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AngularMaterialModule,
+        RouterModule
+    ]
+})
+export class PostModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://NodeAngular-env-1.pvqm4gmmdq.eu-central-1.elasticbeanstalk.com/api'
+};
